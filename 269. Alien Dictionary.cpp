f56e150b1d05edb2733b6f804a1561f9afef41a2
Fanye518 <37570169+Fanye518@users.noreply.github.com>
2019-03-12 12:30:44 -0700

    Update 269. Alien Dictionary.cpp

--- 269. Alien Dictionary.cpp
+++ 269. Alien Dictionary.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string alienOrder(vector<string>& words) {
         unordered_map<char,int> degree;
+        // degree['c']++;
+        // if(degree.find('c')!=degree.end())cout<<degree['c']<<endl;
         for(auto &word:words){
             for(auto &c:word){
                 degree[c]=0;
@@ -25,24 +27,42 @@ public:
         }
         
         string res = "";
-        while(degree.size()){
-            // int n=degree.size();
-            bool b_find = false;
-            char ch;
-            for(auto &p:degree){
-                if(p.second==0){
-                    b_find = true;
-                    ch = p.first;
-                    res +=ch;
-                    for(auto &c:graph[ch]){
+        queue<char>q;
+        for(auto &p:degree) if(p.second==0)
+            q.push(p.first);
+        if(!q.size())return res;
+        while(q.size()){
+            int qsize = q.size();
+            for(int i=0;i<qsize;i++){
+                char ch=q.front();q.pop();
+                res+=ch;
+                for(auto &c:graph[ch]){
                     degree[c]--;
-                    }
-                    degree.erase(ch);
-                    break;
+                    if(!degree[c])q.push(c);
                 }
+                degree.erase(ch);    
             }
-            if(!b_find)return "";
         }
+        if(degree.size())return "";
+        
+        // while(degree.size()){
+        //     // int n=degree.size();
+        //     bool b_find = false;
+        //     char ch;
+        //     for(auto &p:degree){
+        //         if(p.second==0){
+        //             b_find = true;
+        //             ch = p.first;
+        //             res +=ch;
+        //             for(auto &c:graph[ch]){
+        //                 degree[c]--;
+        //             }
+        //             degree.erase(ch);
+        //             break;
+        //         }
+        //     }
+        //     if(!b_find)return "";
+        // }
         return res;
         
     }
